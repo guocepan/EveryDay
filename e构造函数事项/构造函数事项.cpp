24ce52a8f7c7a0af299292020c5c22abf8d9508b
guocepan <1203543128@qq.com>
2017-09-28 21:59:37 +0800

    无聊

--- e构造函数事项/构造函数事项.cpp
+++ e构造函数事项/构造函数事项.cpp
@@ -1,14 +1,17 @@
 //构造函数基本使用
-
+#include<iostream>
+using namespace std;
 class  Widget
 {
 public:
 	Widget();//default 构造函数
 	Widget(const Widget &rhs);//copy构造函数
 	Widget & operator=(const Widget &rhs);//copy assignment操作符
-	...
+	
 };
 
+void main()
+{}
 Widget w1; //调用default构造函数
 Widget w2(w1);//调用copy构造函数
 w1 = w2;  //copy assignment操作符
@@ -17,4 +20,4 @@ w1 = w2;  //copy assignment操作符
 Widget w3 = w2;//调用的是copy构造函数;
 			   //这个是新创建一个对象，必然调用默认构造函数
 			    //不可能调用赋值操作。
- 
+ }
---
+++ word/word.cpp
@@ -0,0 +1,13 @@
+abstract  抽象的
+abstraction   抽象性
+access   访问
+access leave 访问级别
+access function  访问函数
+adapter   适配器
+address   地址
+address-of operator取地址操作符
+aggregation  聚合
+algorithm 算法
+allocate 分配
+allocator 分配器
+
