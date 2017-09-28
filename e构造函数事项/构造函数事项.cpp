aeb0e4811ac29bbbde56c743bce2faa76744beae
guocepan <1203543128@qq.com>
2017-09-28 21:41:26 +0800

    构造函数基础

---
+++ e构造函数事项/构造函数事项.cpp
@@ -0,0 +1,20 @@
+//构造函数基本使用
+
+class  Widget
+{
+public:
+	Widget();//default 构造函数
+	Widget(const Widget &rhs);//copy构造函数
+	Widget & operator=(const Widget &rhs);//copy assignment操作符
+	...
+};
+
+Widget w1; //调用default构造函数
+Widget w2(w1);//调用copy构造函数
+w1 = w2;  //copy assignment操作符
+
+//这个要小心
+Widget w3 = w2;//调用的是copy构造函数;
+			   //这个是新创建一个对象，必然调用默认构造函数
+			    //不可能调用赋值操作。
+
