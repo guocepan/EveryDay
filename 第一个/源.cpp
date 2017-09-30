b4426113cb853e69a7ad87a314a8138a4e8a5dd5
guocepan <1203543128@qq.com>
2017-09-30 09:52:45 +0800

    第一个win32程序

---
+++ 第一个/源.cpp
@@ -0,0 +1,12 @@
+#include<windows.h>
+
+int WINAPI  WinMain(HINSTANCE  hInstance, HINSTANCE  hPrevInstance,
+	PSTR  szCmdLine, int iCmdShow)
+{
+
+	MessageBox(NULL,TEXT("hello window 98"),TEXT("hello msg"),0);
+
+	return 0;
+
+}
+
